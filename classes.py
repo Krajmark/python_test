6a4416d4de294e66b3519d69aff3bff5301b26d8
Krajmark <kmark.mrkk@gmail.com>
2024-05-20 18:23:31 +0200

    INHERETANCE/super method

--- classes.py
+++ classes.py
@@ -1,9 +1,14 @@
-class Car:
+class Veichle:
+    def __init__(self, tire_count, initial_fuel):
+        self.tire_count = tire_count
+        self.fuel = initial_fuel
+        
+class Car(Veichle):
     
     isOn = False
      
     def __init__(self, starting_fuel = 100):
-        self.fuel = starting_fuel
+        super().__init__(4, starting_fuel)
     
     def honk(self):
         if self.isOn:
@@ -18,3 +23,4 @@ car_1.isOn = True
 car_1.honk()
 print(car_1.fuel)
 print(car_2.fuel)
+print(car_1.tire_count)
