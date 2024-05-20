8509941a030f0417d32cddc16ff65b7c5fbfc482
Krajmark <kmark.mrkk@gmail.com>
2024-05-20 18:09:51 +0200

    dunder methods

--- classes.py
+++ classes.py
@@ -1,6 +1,9 @@
 class Car:
-    fuel = 100
+    
     isOn = False
+     
+    def __init__(self, starting_fuel = 100):
+        self.fuel = starting_fuel
     
     def honk(self):
         if self.isOn:
@@ -9,6 +12,9 @@ class Car:
             print('...')
             
 car_1 = Car()
+car_2 =Car(200)
 car_1.honk()
 car_1.isOn = True
 car_1.honk()
+print(car_1.fuel)
+print(car_2.fuel)
