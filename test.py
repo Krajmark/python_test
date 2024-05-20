07131a6c3169167f8126d7bffe78668f1958026a
Krajmark <kmark.mrkk@gmail.com>
2024-05-20 17:23:29 +0200

    f-string

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-print('ASD')
-print(20 * 0.75)
+# print('ASD')
+# print(20 * 0.75)
+
+user_name = input('What is your name?\n')
+user_age = input('How old are you?(yrs)')
+print(f'Hi {user_name} you are {user_age} years old.')
